7ea0c6734e453029700a5e747ec52acb6949512b
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2021-10-22 16:56:15 +0700

    update N+1 Problem solved

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-    
+<h1 class="mb-5">{{ $tittle }}</h1>
+
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-4">
             <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->tittle }}</h2></a>
