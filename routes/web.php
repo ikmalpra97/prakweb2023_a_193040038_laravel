78abd20b2e8759cedd8a2036f19c75fe53bdd419
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2021-11-18 19:18:43 +0700

    Update View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
+
 use App\Models\Category;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,4 +48,7 @@ Route::get('/categories', function() {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
 
