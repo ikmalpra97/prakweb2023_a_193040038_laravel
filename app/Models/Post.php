90d7444564858919442726ec41df96bb09207df9
ikmalpra97 <193040038.ilham@mail.unpas.ac.id>
2021-11-18 18:12:17 +0700

    update searching & pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,28 @@ class Post extends Model
     // protected $fillable = ['tittle', 'excerpt','body'];
     protected $guarded =['id'];
     protected $with = ['category','author'];
+
+    public function scopeFilter($query, array $filters){
+        //callback
+        $query->when($filters['search'] ?? false, function($query, $search){
+            return $query ->where('tittle', 'like', '%' . $search . '%')
+                          ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        //callback
+        $query->when($filters['category'] ?? false, function($query, $category) {
+            return $query->whereHas('category', function($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        //arrow function
+        $query->when($filters['author'] ?? false, fn($query, $author) => 
+                $query->whereHas('author', fn($query) =>
+                $query->where('username', $author)
+            )
+        );
+    }
     
     public function category()
     {
--- routes/web.php
+++ routes/web.php
@@ -48,17 +48,3 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'tittle' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category','author'),
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts', [
-        'tittle' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category','author')
-    ]);
-});
