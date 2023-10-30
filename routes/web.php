2c80a2cd5dc2030366456dc24c5db125aa912789
unknown <193040038.ilham@mail.unpas.ac.id>
2023-10-30 21:51:08 +0700

    Menyelesaikan video 18

--- routes/web.php
+++ routes/web.php
@@ -73,3 +73,7 @@ Route::get('/dashboard', function(){
 })->middleware('auth'));
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
