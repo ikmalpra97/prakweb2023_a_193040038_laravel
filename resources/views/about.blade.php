ca72705812640006ad861660a84445b81c8f14b8
Rafid <Rafidrb0@gmail.com>
2023-12-31 23:28:20 +0700

    test push

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman About</h1>
+    <h1>Halaman Abouta</h1>
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
     <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
